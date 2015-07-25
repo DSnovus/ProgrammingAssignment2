50209d739136df708f7c38bb0e28de4ba9fa32ef
DSnovus <russianhoya@gmail.com>
2015-07-25 17:11:26 -0400

    Final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## makeCacheMatrix creates a list which contains the value of a matrix and the value of inverse of the matrix.
 ## cacheMatrix calculates the inverse of the matrix created with the above function. It first checks if the inverse
 ## has already been calculated. If it has, the function returns the inverse from the cache and skips the computation.
-## Otherwise it calculate the inverse and sets the value of the inverse matrix witht he setsolve function. 
+## Otherwise, it calculate the inverse and sets the value of the inverse matrix witht he setsolve function. 
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
