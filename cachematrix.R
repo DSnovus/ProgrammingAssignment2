165968fd1b164fa70af756677db35d98375afbae
DSnovus <russianhoya@gmail.com>
2015-07-25 17:07:51 -0400

    Revert "Coursera"
    This reverts commit 2bfbbb47f6508f2944c13aaa6967bd69a3c33742.

--- cachematrix.R
+++ cachematrix.R
@@ -1,36 +1,15 @@
-## makeCacheMatrix creates a list which contains the value of a matrix and the value of inverse of the matrix.
-## cacheMatrix calculates the inverse of the matrix created with the above function. It first checks if the inverse
-## has already been calculated. If it has, the function returns the inverse from the cache and skips the computation.
-## Otherwise it calculate the inverse and sets the value of the inverse matrix witht he setsolve function. 
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
-  list(set = set, get = get,
-       setsolve = setsolve,
-       getsolve = getsolve)
+
 }
 
 
-## cacheSolve checks if the  inverse has alreayd been calculated. If it has, the function returns the inverse from the cache and skips the computation.
-## Otherwise it calculate the inverse and sets the value of the inverse matrix witht he setsolve function. 
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  m <- x$getsolve()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
-  m
-  
-  ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of 'x'
 }
